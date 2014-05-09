f038ccb9070f3c9ac5d79977486f6144cc6db479
March <March@Marchs-MacBook-Pro.local>
2014-05-09 21:04:21 +0800

    final version

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
-require 'sinatra'
+  require 'sinatra'
 require './boot.rb'
+require './money_calculator'
 
 # ROUTES FOR ADMIN SECTION
 get '/admin' do
@@ -43,3 +44,48 @@ get '/delete_product/:id' do
   redirect to '/admin'
 end
 # ROUTES FOR ADMIN SECTION
+#my code
+get '/' do
+  @products = Item.all
+  erb :home
+end
+
+get '/about' do
+  erb :about
+end
+
+get '/product' do
+  @products = Item.all
+  erb :product
+end
+
+get '/purchase/:id' do
+  @product = Item.find(params[:id])
+  erb :purchase_form
+end
+
+post '/result/:id' do
+  @product = Item.find(params[:id])
+  @cost = params[:amount].to_i * @product.price
+  @money = MoneyCalculator.new(params[:ones].to_i, params[:fives].to_i, params[:tens].to_i, params[:twenties].to_i, params[:fifties].to_i, params[:hundreds].to_i, params[:five_hundreds].to_i, params[:thousands].to_i)
+  if params[:amount].to_i>0
+    if @money.total >= @cost 
+      if @product.quantity.to_i >= params[:amount].to_i
+        @hash = @money.change(@cost)
+        new_quantity = @product.quantity - params[:amount].to_i
+        new_sold = @product.sold + params[:amount].to_i
+        @product.update_attributes!(
+        quantity: new_quantity,
+        sold: new_sold
+        )
+        erb :result_true
+      else
+        erb :result_false_quantity
+    end
+  else
+    erb :result_false_money
+  end
+else
+    erb :result_false_amount
+end
+end
--- money_calculator.rb
+++ money_calculator.rb
@@ -1,12 +1,68 @@
 class MoneyCalculator
+	attr_accessor :ones, :fives, :tens, :twenties, :fifties, :hundreds, :five_hundreds, :thousands
 
   def initialize(ones, fives, tens, twenties, fifties, hundreds, five_hundreds, thousands)
+  	@ones = ones
+  	@fives = fives
+  	@tens = tens
+  	@twenties = twenties
+  	@fifties = fifties
+  	@hundreds = hundreds
+  	@five_hundreds = five_hundreds
+  	@thousands = thousands
+    @total = @ones + (@fives*5) + (@tens*10) + (@twenties*20) + (@fifties*50) + (@hundreds*100) + (@five_hundreds*500) + (@thousands*1000)
     # each parameter represents the quantity per denomination of money
     # these parameters can be assigned to instance variables and used for computation
 
-    # add a method called 'change' that takes in a parameter of how much an item costs
+    # add a method called 'change' that takes in a parameter of how much an item @changes
     # and returns a hash of how much change is to be given
     # the hash will use the denominations as keys and the amount per denomination as values
     # i.e. {:fives => 1, fifties => 1, :hundreds => 3}
   end
+
+  def total
+    @total
+  end
+
+  def change(cost)
+  
+    @change = @total - cost
+
+  until @change == 0
+
+      if @change >= 1000
+        @th = @change/1000
+        @change = @change%1000
+      elsif @change >= 500
+        @fh = @change/500
+        @change = @change%500
+      elsif @change >= 100
+        @hu = @change/100
+        @change = @change%100
+      elsif @change >= 50
+        @ft = @change/50
+        @change = @change%50
+      elsif @change >= 20
+        @tw = @change/20
+        @change = @change%20
+      elsif @change >= 10
+        @tn = @change/10
+        @change = @change%10
+      elsif @change >= 5
+        @fv = @change/5
+        @change = @change%5
+      else
+        @on = @change 
+        @change = 0  
+    end
+  end
+    @return = {
+      :ones => @on, :fives => @fv, :tens => @tn, :twenties => @tw, :fifties => @ft, 
+      :hundreds => @hu, :five_hundreds => @fh,  :thousands => @th}
+  end
+
+    def cng
+
+    @change  
+    end
 end
